9077785549902bb51a20dd196aa9ee48ea7f4df3
Asneh Raj <rajasneh91@gmail.com>
2024-09-17 08:45:06 +0530

    Export in files

---
+++ Backend-1-Nodejs/Export_in_Files01.js
@@ -0,0 +1,14 @@
+const sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const pi=3.14;
+
+let obj={          // it is a object so we add function or properties to it
+    sum :sum,
+    mul :mul,
+    pi :pi,
+};
+module.exports=obj;
+
+//second method to export
+
+//exports.subtract=(a,b)=>a-b;
---
+++ Backend-1-Nodejs/Export_in_Files02.js
@@ -0,0 +1,5 @@
+const math=require("./Export_in_Files01"); //require the properties
+
+console.log(math.sum(5,2));
+console.log(math.pi);
+console.log(math.mul(2,7));
