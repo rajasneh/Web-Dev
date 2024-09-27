817519a2e98ee249bf327741937c61d09dd68029
Asneh Raj <rajasneh91@gmail.com>
2024-09-27 16:04:13 +0530

    creating ids (UUIDs)

--- Backend-4-REST/index.js
+++ Backend-4-REST/index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const { v4: uuidv4} = require('uuid');
 
 app.use(express.urlencoded({extended:true}));
 
@@ -11,14 +12,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts=[
     {
+        id:uuidv4(),
         username:"asnehraj",
         content:"I love gaming"
     },
     {
+        id:uuidv4(),
         username:"jellySinghi",
         content:"I love to watch movies",
     },
     {
+        id:uuidv4(),
         username:"amardeepverma",
         content:"I love coding",
     }
@@ -39,6 +43,12 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let{username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let post=posts.find((p)=>p.id===id)
+    res.render("show.ejs",{post});
 });
