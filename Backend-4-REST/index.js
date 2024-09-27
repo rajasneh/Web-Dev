dd4bd3bf31711ce86b96c12f048e5a52c7962e00
Asneh Raj <rajasneh91@gmail.com>
2024-09-27 22:56:11 +0530

    Edit route

--- Backend-4-REST/index.js
+++ Backend-4-REST/index.js
@@ -3,8 +3,10 @@ const app=express();
 const port=8080;
 const path=require("path");
 const { v4: uuidv4} = require('uuid');
+var methodOverride = require('method-override');
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -49,6 +51,20 @@ app.post("/posts",(req,res)=>{
 });
 app.get("/posts/:id",(req,res)=>{
     let{id}=req.params;
-    let post=posts.find((p)=>p.id===id)
+    let post=posts.find((p)=>id===p.id);
     res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let newContent=req.body.content;
+    let post=posts.find((p)=>id===p.id);
+    post.content=newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
 });
