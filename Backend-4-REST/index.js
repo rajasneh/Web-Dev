781640537ad1880c04cd8f6bfd16714c48ff43df
Asneh Raj <rajasneh91@gmail.com>
2024-09-28 08:08:58 +0530

    Delete

--- Backend-4-REST/index.js
+++ Backend-4-REST/index.js
@@ -68,3 +68,8 @@ app.get("/posts/:id/edit",(req,res)=>{
     let post=posts.find((p)=>id===p.id);
     res.render("edit.ejs",{post});
 });
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=>id!=p.id);
+    res.redirect("/posts");
+})
