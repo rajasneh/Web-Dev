9cadbe775be73c150e78b37ab0bb98a3b0623ab1
Asneh Raj <rajasneh91@gmail.com>
2024-10-01 23:34:13 +0530

    Using faker and mysql package

---
+++ Backend-5-Node-with-SQL/MySQL_Package.js
@@ -0,0 +1,28 @@
+const {faker}=require("@faker-js/faker");
+const mysql=require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'faker',
+  password:/*"Your Password"*/"",
+});
+
+try{
+  connection.query("SHOW TABLES", (err,result)=>{
+    if(err) throw err;
+    console.log(result);
+  });
+} catch(err){
+  console.log(err);
+}
+
+connection.end();
+let  getRandomUser= ()=> {
+    return {
+      id: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
