4e82ad79724a606620009dd033e34c4cf177f329
Asneh Raj <rajasneh91@gmail.com>
2024-10-06 17:10:16 +0530

    Create table user and Insert into user

---
+++ Backend-5-Node-with-SQL/Insert_Into_user.js
@@ -0,0 +1,35 @@
+const mysql=require('mysql2');
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database:"delta_app",
+    password:"Asneh@Raj281104"
+});
+
+let q="INSERT INTO user (id,username,email,password) VALUES ?";
+
+let users=[
+    ["123b","abcb","abc@gmail.comb","abcb"],
+    ["123a","abca","abc@gmail.coma","abca"],
+];
+
+
+try{
+    connection.query(q,[users],(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+    });
+} catch(err){
+    console.log(err);
+}
+
+connection.end();
+let getRandomUser= ()=> {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
