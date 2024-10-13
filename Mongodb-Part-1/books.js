2d697ff0a7c912ce3c34bae77549974a686b50d6
Asneh Raj <rajasneh91@gmail.com>
2024-10-13 11:41:40 +0530

    Scheam validatios and schema type options

--- Mongodb-Part-1/books.js
+++ Mongodb-Part-1/books.js
@@ -20,19 +20,29 @@ const bookSchema=new mongoose.Schema({
     price:{
         type:String,
     },
+    discount:{       //Schema type option
+        type:Number,
+        default:0
+    },
+    category:{
+        type:String,
+        enum:["fiction","non-fiction"]
+    },
+    genre:[String]
 });
 
 const book =mongoose.model("book",bookSchema);
 
 let book1=new book({
-    title:"How to kill a Mockingbird",
-    author:"HArper Lee",
-    price:900,
+    title:"Marvel Comics",
+    price:799,
+    category:"fiction",
+    genre:["superheroes","drama","comics"]
 });
 
-// book1.save().then((res)=>{
-//     console.log(res);
-// })
-// .catch((err)=>{
-//     console.log(err);
-// })
+book1.save().then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+})
