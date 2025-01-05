6ddb226dc2337d2817d524a12fb46255a67cbc9a
Asneh Raj <rajasneh91@gmail.com>
2025-01-05 20:34:42 +0530

    Structuring Components

--- React/React-Part-1/basic-react-app/src/App.jsx
+++ React/React-Part-1/basic-react-app/src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
 import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx"
+const name="Asneh";
 
+// function App() {
+//   return (
+//     //  React fragment
+//     <>
+//       <Title/>
+//       {/* JSX  with curly braces */}
+//       <p>Hi {name}</p>
+//       <p>2 x 2 = {2 * 2}</p>
+//       <Title/>
+//     </>
+    
+//   );
+
+// }
 
 
 function App() {
   return (
-    <div>
-    <Title/>
-    <Title/>
-    </div>
+    <ProductTab/>
   );
 
 }
---
+++ React/React-Part-1/basic-react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product() {
+    return (
+        <div>
+            <h3>Product Title</h3>
+            <h5>Produc Description</h5>
+        </div>
+    );
+}
+
+export default Product;
