97c8a59bef3ed67255bbf894b59a9df0141b8049
Asneh Raj <rajasneh91@gmail.com>
2025-01-07 19:43:22 +0530

    Handling Click events and non click events

---
+++ React/React-Part-3/basic-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import  "./App.css"
+import Button from "./Button.jsx"
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ React/React-Part-3/basic-react-app/src/Button.jsx
@@ -0,0 +1,23 @@
+function handleClick() {
+  console.log("Hello!");
+}
+function handleMouseOver() {
+  console.log("Bye!");
+}
+function handleDobuleClick(){
+    console.log("You dobule clicked");
+}
+export default function Button() {
+  return (
+    <div>
+      <button onClick={handleClick}> Click me!</button>
+      <p onMouseOver={handleMouseOver}>
+        <b>his function is for Event Demo</b>Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quasi eius alias quos consequuntur,
+        repellat quae vel officia repudiandae? Ullam voluptatem, cumque aliquam
+        repellat quos quibusdam itaque vero iure officiis vel!
+      </p>
+      <button onDoubleClick={handleDobuleClick}>Dobule click me!</button>
+    </div>
+  );
+}
