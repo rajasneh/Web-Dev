ca3d045eca474eb016bc738dc721e70596be5a25
Asneh Raj <rajasneh91@gmail.com>
2025-01-09 21:39:06 +0530

    useState() & Hooks

---
+++ React/React-Part-3/react-state/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counter from "./Counter.jsx";
+function App() {
+  return (
+    <>
+      <h1>States in react</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
---
+++ React/React-Part-3/react-state/src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter() {
+    let [count,setCount]=useState(0);
+
+  function inCount() {
+    setCount(count+1);
+  }
+  return (
+    <div>
+      <h3>Count = {count}</h3>
+      <button onClick={inCount}>Increase Count</button>
+    </div>
+  );
+}
