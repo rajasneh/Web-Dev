9a3db7871825de306881ac92bf430fdff31f4e03
Asneh Raj <rajasneh91@gmail.com>
2025-01-17 19:43:25 +0530

    Function as props

--- React/React-Part-5/lottery-game/src/App.jsx
+++ React/React-Part-5/lottery-game/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Lottery from "./Lottery"
+import "./App.css";
+import Lottery from "./Lottery";
+import { sum } from "./helper";
 
 function App() {
+  let winCondition = (ticket) => {
+    return sum(ticket) === 15;
+  };
   return (
     <>
-     <Lottery n={3} winningSum={15}/>
+      <Lottery n={3} winCondition={winCondition} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- React/React-Part-5/lottery-game/src/Lottery.jsx
+++ React/React-Part-5/lottery-game/src/Lottery.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { genTicket ,sum} from "./helper.js";
 import Ticket from "./Ticket.jsx";
 
-export default function Lottery({n=3,winningSum=15}) {
+export default function Lottery({n=3,winCondition}) {
   let [ticket, setTicket] = useState(genTicket(3));
-  let isWinner=sum(ticket)===winningSum;
+  let isWinner=winCondition(ticket);
 
   let buyTicket=()=>{
     setTicket(genTicket(n));
