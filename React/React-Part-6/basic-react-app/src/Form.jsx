f466b1adabbf288f7aa5b089ac224bb5e8f228b1
Asneh Raj <rajasneh91@gmail.com>
2025-01-23 08:46:57 +0530

    Handling multiple inputs

--- React/React-Part-6/basic-react-app/src/Form.jsx
+++ React/React-Part-6/basic-react-app/src/Form.jsx
@@ -1,19 +1,52 @@
 import { useState } from "react";
 export default function Form() {
-  let [fullName, setFullName] = useState("Asneh");
+  // let [fullName, setFullName] = useState("Asneh");
+
+  // let handleChange = (event) => {
+  //   setFullName(event.target.value);
+  // };
+
+  //Handling Multiple Inputs
+  let [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+  });
 
   let handleChange = (event) => {
-    setFullName(event.target.value);
+    //common handler for all input field
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefalut();
+    setFormData({
+      fullName: "",
+      username: "",
+    });
   };
 
   return (
     <form>
-      <label htmlFor="userName">Full Name</label>
+      <label htmlFor="fullName">Full Name</label>
       <input
         placeholder="enter your name"
         type="text"
-        value={fullName}
-        id="userName"
+        value={formData.fullName}
+        id="fullName"
+        name="fullName"
+        onChange={handleChange}
+      ></input>
+      <br></br>
+      <br></br>
+      <label htmlFor="username">Username</label>
+      <input
+        placeholder="enter your username"
+        type="text"
+        value={formData.username}
+        id="username"
+        name="username"
         onChange={handleChange}
       ></input>
       <button>Submit</button>
