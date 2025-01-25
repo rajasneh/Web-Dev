f6d7853cb7ab2c8640cdcda07c96f92aa445b8a2
Asneh Raj <rajasneh91@gmail.com>
2025-01-25 19:33:35 +0530

    useeffect dependencies

--- React/React-Part-6/basic-react-app/src/Counter.jsx
+++ React/React-Part-6/basic-react-app/src/Counter.jsx
@@ -1,17 +1,26 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 
-export default function Counter(){
-    let [count,setCount]=useState(0);
-    let incCount=()=>{
-        setCount((curCount)=>curCount+1);
-   }
-   useEffect(function printSomething(){
-    console.log("This is the side effect");
-   })
-   return (
+export default function Counter() {
+  let [countx, setCountx] = useState(0);
+  let [county, setCounty] = useState(0);
+  let incCountx = () => {
+    setCountx((curCount) => curCount + 1);
+  };
+  let incCounty = () => {
+    setCounty((curCount) => curCount + 1);
+  };
+  useEffect(
+    function printSomething() {
+      console.log("This is the side effect");
+    },
+    [countx]
+  ); //what we put inside a square bracket only in that state the useeffect will work if empty only in rendering first time it will render
+  return (
     <div>
-        <h3>count={count}</h3>
-        <button onClick={incCount}>+1</button>
+      <h3>countx={countx}</h3>
+      <button onClick={incCountx}>+1</button>
+      <h3>county={county}</h3>
+      <button onClick={incCounty}>+1</button>
     </div>
-   )
+  );
 }
